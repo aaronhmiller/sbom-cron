62703915fd68bfa3f75e86740dea666cd06ebb9d
Aaron Miller <aaronmiller@gmail.com>
2024-08-03 18:41:19 -0700

    fixing syntax

--- cron.ts
+++ cron.ts
@@ -1,10 +1,10 @@
-.---------------- minute (0 - 59)
-| .-------------- hour (0 - 23)
-| | .------------ day of month (1 - 31)
-| | | .---------- month (1 - 12) OR jan,feb,mar ...
-| | | | .-------- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue ...
-| | | | |
-* * * * * command to be executed
+//.---------------- minute (0 - 59)
+//| .-------------- hour (0 - 23)
+//| | .------------ day of month (1 - 31)
+//| | | .---------- month (1 - 12) OR jan,feb,mar ...
+//| | | | .-------- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue ...
+//| | | | |
+//* * * * * command to be executed
 
 Deno.cron("Sample cron job", "0 */2 * * *", () => {
   console.log("This will run twelve times a day");
