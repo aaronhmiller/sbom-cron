9231069fdf46c6148d93c458e50dfcb1ce748f34
Aaron Miller <aaronmiller@gmail.com>
2024-08-03 18:40:17 -0700

    adding comment and changing to 12x daily

--- cron.ts
+++ cron.ts
@@ -1,3 +1,11 @@
-Deno.cron("Sample cron job", "0 0 * * *", () => {
-  console.log("This will run once a day");
+.---------------- minute (0 - 59)
+| .-------------- hour (0 - 23)
+| | .------------ day of month (1 - 31)
+| | | .---------- month (1 - 12) OR jan,feb,mar ...
+| | | | .-------- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue ...
+| | | | |
+* * * * * command to be executed
+
+Deno.cron("Sample cron job", "0 */2 * * *", () => {
+  console.log("This will run twelve times a day");
 });
